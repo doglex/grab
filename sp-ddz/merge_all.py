a60881b39fd02760729d5f342d7f0eff510e14b3
Dogless <awsxsa@hotmail.com>
2023-02-05 20:01:00 +0800

    ddz: merge_all

--- sp-ddz/merge_all.py
+++ sp-ddz/merge_all.py
@@ -7,7 +7,7 @@ dir_from = "store"
 files = listdir(dir_from)
 
 df_collector = []
-for i, filename in enumerate(files[:10]):
+for i, filename in enumerate(files[:]):
     print(f"{datetime.now()} => {i+1}/{len(files)}: {filename}")
     if not filename.endswith(".csv"):
         continue
@@ -15,7 +15,14 @@ for i, filename in enumerate(files[:10]):
     # print(df)
     df_collector.append(df)
 
+
+
 df_all = pd.concat(df_collector, axis=0)
 print(df_all)
-df_all.to_excel("蛋蛋赞总索引.xlsx")
+shape = df_all.shape[0]
+size = 65530
+for i, step in enumerate(range(0, shape, size)):
+    print(f"write part {i+1}")
+    df_tmp = df_all.iloc[step:step+size]
+    df_tmp.to_excel(f"蛋蛋赞总索引_part_{i+1}.xlsx", engine="xlsxwriter")
 print("done!")
